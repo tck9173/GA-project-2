7272862aee01c6a2a9a2b9865fad53cdea61d2de
MD Manning <manningmichaeld@johndeere.com>
2020-10-02 15:40:28 -0500

    basic template setup

---
+++ server.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+
+const express = require('express');
+const methodOverride=require('method-override');
+const app = express();
+const routes = require('./routes')
+
+app.use(express.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
+app.use(express.static('public'))
+
+
+
+
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`I am listening on  ${process.env.PORT}`);
+})
+
+
