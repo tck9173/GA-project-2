b3dc0ea68ed84116541882d82f3f34e6bcf2afbf
Tim Kaiser <kaisertimothy@johndeere.com>
2020-10-05 16:48:06 -0500

    Updated gifting

--- controllers/friends.js
+++ controllers/friends.js
@@ -3,12 +3,20 @@ const Users = require('../models').Users;
 const Friend = require('../models').Friend;
 
 const addFriend = (req, res) => {
-    req.body.relationship = 1;
-    req.body.userId1 = req.params.index;
-    Friend.create(req.body)
-    .then(newFriend => {
-        res.redirect(`/users/profile/${req.params.index}`)
-    })     
+    req.body.userId = req.body.userId2
+    Crops.update(req.body, {
+        where: {id:req.body['crop.id']},
+        returning: true
+    })
+    .then(updatedCrop => {
+        req.body.relationship = 1;
+        req.body.userId1 = req.params.index;
+        Friend.create(req.body)
+        .then(newFriend => {
+            res.redirect(`/users/profile/${req.params.index}`)
+        })    
+    })
+         
 }
 
 
