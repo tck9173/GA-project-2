9a4ac3d66e6f58e26b5de211bb41da4400d54786
MD Manning <manningmichaeld@johndeere.com>
2020-10-05 12:49:56 -0500

    yup

---
+++ migrations/20201005161350-create-friend.js
@@ -0,0 +1,35 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Friends', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userId1: {
+        type: Sequelize.INTEGER
+      },
+      userId2: {
+        type: Sequelize.INTEGER
+      },
+      relationship: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        defaultValue: new Date(),
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        defaultValue: new Date(),
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Friends');
+  }
+};
