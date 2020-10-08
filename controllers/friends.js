c8096301f5f701447c92f09e8850e1b5813ccab2
Tim Kaiser <kaisertimothy@johndeere.com>
2020-10-08 11:33:22 -0500

    updated game

--- controllers/friends.js
+++ controllers/friends.js
@@ -43,36 +43,45 @@ const addFriend = (req, res) => {
 
 const giveGift = (req, res) => {
     req.body.userId = req.body.userId2
-    Crops.update(req.body, {
-        where: {id:req.body['crop.id']},
-        returning: true
+    Crops.findOne({
+        where: {id:req.body['crop.id']}
     })
-    .then(updatedCrop => {
-        req.body.userId1 = req.params.index;
-        Friend.findOne({
-            where: {
-                userId1: req.params.index,
-                userId2: req.body.userId2
-            }
-            //add user id 2 constraint
+    .then(foundCrop => {
+        Crops.update(req.body, {
+            where: {id:req.body['crop.id']},
+            returning: true
         })
-        //need to find specific relationship
-        //where: userId1 = req.params.index
-        .then(foundRelationship => {
-            req.body.relationship = foundRelationship.relationship + 1;
-            Friend.update(req.body, {
-                where: { 
+        .then(updatedCrop => {
+            req.body.userId1 = req.params.index;
+            Friend.findOne({
+                where: {
                     userId1: req.params.index,
                     userId2: req.body.userId2
-                },
-                returning: true,
+                }
             })
-            .then(updatedFriend => {
-                res.redirect(`/users/profile/${req.params.index}`)
-            })    
+            .then(foundRelationship => {
+                if (foundCrop.quality === 'low') {
+                    req.body.relationship = foundRelationship.relationship + 1;
+                } else if (foundCrop.quality === 'medium') {
+                    req.body.relationship = foundRelationship.relationship + 2;
+                } else if (foundCrop.quality === 'high') {
+                    req.body.relationship = foundRelationship.relationship + 3;
+                }
+                Friend.update(req.body, {
+                    where: { 
+                        userId1: req.params.index,
+                        userId2: req.body.userId2
+                    },
+                    returning: true,
+                })
+                .then(updatedFriend => {
+                    res.redirect(`/users/profile/${req.params.index}`)
+                })    
+            })
+                
         })
-            
     })
+    
 }
 
 module.exports = {
